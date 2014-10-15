8dc3fdbf5f0c50261fba6828be993bb3d525580d
Stéphane Gully <stephane.gully@gmail.com>
2014-10-15 18:57:53 +0200

    improve styles

--- bibliomap.js
+++ bibliomap.js
@@ -11,29 +11,28 @@ socket.on('ezpaarse-ec', function (ec) {
 BibliomapOverlay.prototype = new google.maps.OverlayView();
 
 var ezproxyColor = {
-  'bibliovie': { 'color': 'red', 'name': 'Science de la Vie'},
-  'titanesciences': { 'color': 'yellow', 'name': 'Sciences Chimiques'},
-  'biblioinserm': { 'color': 'blue', 'name': 'INSERM'},
-  'bibliost2i': {'color': 'green', 'name': 'Sciences et Techniques de l\'ingenieur' },
-  'biblioshs': { 'color': 'brown', 'name': 'Sciences Humaines et Sociales'}
+  'bibliovie': { 'color': '#FF7777', 'name': 'Science de la Vie'},
+  'titanesciences': { 'color': '#FFED77', 'name': 'Sciences Chimiques'},
+  'biblioinserm': { 'color': '#9094FF', 'name': 'INSERM'},
+  'bibliost2i': {'color': '#ACFFA1', 'name': 'Sciences et Techniques de l\'ingenieur' },
+  'biblioshs': { 'color': '#CEB180', 'name': 'Sciences Humaines et Sociales'}
 };
 
-  var cList = $('<ul></ul>')
-      .css('top', '250px')
-      .css('left', '100px')
-      .css('z-index', '99')
-      .css('position', 'absolute');
+  // var cList = $('<ul></ul>')
+  //     .css('top', '250px')
+  //     .css('left', '100px')
+  //     .css('z-index', '99')
+  //     .css('position', 'absolute');
 
-  Object.keys(ezproxyColor).forEach(function(k)
-  {
-    console.log(k);
-    var li = $('<li/>')
-        .text(ezproxyColor[k].name)
-        .css('background-color', ezproxyColor[k].color)
-        .appendTo(cList);
-  });
-  console.log(cList);
-  $('#bibliomap-canvas').append(cList);
+  // Object.keys(ezproxyColor).forEach(function(k) {
+  //   console.log(k);
+  //   var li = $('<li/>')
+  //       .text(ezproxyColor[k].name)
+  //       .css('background-color', ezproxyColor[k].color)
+  //       .appendTo(cList);
+  // });
+  // console.log(cList);
+  // $('#bibliomap-canvas').append(cList);
 
 function initialize () {
   var mapOptions = {
@@ -85,34 +84,53 @@ BibliomapOverlay.prototype.addEzpaarseEC = function (ec) {
 
   // Create the DIV and set some basic attributes.
   ec.div = $('<div></div>');
-  //ec.div.css('border', '1px solid blue')
+  // ec.div.css('border', '1px solid blue')
+  ec.div.css('border', '1px solid transparent')
   ec.div.css('position', 'absolute');
   ec.div.css('color', '#FFF');
   ec.div.css('font-size', '16px');
   ec.div.css('width', '128px');
   ec.div.css('height', '128px');
-//console.log(ezproxyColor[ec.ezproxyName]);
 
-  var label  = $('<div></div>').text(ec.platform_name)
+  var label  = $('<div></div>').text(ec.ezproxyName)
                                .css('border', '1px solid #000')
-                               .css('vertical-align', 'middle')
                                .css('text-align', 'center')
                                .css('position', 'absolute')
                                .css('background-color', '#FFF')
                                .css('color', '#000')
                                .css('top', '55px')
                                .css('z-index', '100')
+                               .css('padding', '4px').css('margin', '0px').css('line-height', '12px')
                                .css('width', '128px');
+  var extraLabel   = $('<span></span>').text(ec.platform_name)
+                                       .css('font-size', '10px');
+  if (ec.publication_title) {
+    if (ec.publication_title.length > 22) {
+      ec.publication_title = ec.publication_title.substring(0, 22) + '...';      
+    }
+    var extraLabel2  = $('<span></span>').text(ec.publication_title)
+                                         .css('font-size', '10px');
+  }
+  var id = ec.online_identifier ? ec.online_identifier : ec.print_identifier;
+  var extraLabel3 = $('<span></span>').text(ec.rtype + ' | ' + ec.mime + (id ? ' | ' + id : ''))
+                                      .css('font-size', '10px');
+  label.append('<br/>');
+  label.append(extraLabel);
+  if (extraLabel2) {
+    label.append('<br/>');
+    label.append(extraLabel2);
+  }
+  label.append('<br/>');
+  label.append(extraLabel3);
+
+
   var circle = $('<div></div>')
-                               .css('position', 'absolute') 
-                               .css('top', '45px')
-                               .css('left', '40px')
-                               .css('text-align', 'center')
-                               .css('vertical-align', 'middle')
+                               .css('width', '128px')
+                               .css('height', '128px')
+                               .css('margin', 'auto')
+                               .css('margin-top', '35%')
                                .css('border-radius', '50%')
-                               .css('background-color', ezproxyColor[ec.ezproxyName] ? (ezproxyColor[ec.ezproxyName].color || 'blue') : 'blue') 
-                               .css('width', '64px')
-                               .css('height', '64px');
+                               .css('background-color', ezproxyColor[ec.ezproxyName] ? (ezproxyColor[ec.ezproxyName].color || 'blue') : 'blue');
 
   ec.div.append(label);
   ec.div.append(circle);
@@ -129,11 +147,11 @@ BibliomapOverlay.prototype.addEzpaarseEC = function (ec) {
   $(circle).animate({
     width: [ "toggle" ],
     height: [ "toggle" ]
-  }, 3000);
+  }, 8000);
 
   $(ec.div).animate({
     opacity: "toggle",
-  }, 3000);
+  }, 8000);
 
 
   // destroy EC after 5 seconds
