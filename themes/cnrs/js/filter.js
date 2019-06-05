989385989dca416d52f514c0677ce58927458929
Clément Reszetko <clement.reszetko@inist.fr>
2019-06-05 14:27:36 +0200

    changed logo, added meta for html

--- themes/cnrs/js/filter.js
+++ themes/cnrs/js/filter.js
@@ -14,18 +14,22 @@ function counter(ec, portal) {
   }
   const isDisabled = disabledInstitutes.find(institut => institut === ec.ezproxyName);
   if (ec.mime === 'HTML') {
+    // if the institute is enabled : update total html counter
     if (!isDisabled) {
       extCount.html += 1;
       $('#extHTML').html(extCount.html.toLocaleString());
     }
+    // update institute html counter
     portal.html += 1;
   }
 
   if (ec.mime === 'PDF') {
+    // if the institute is enabled : update total pdf counter
     if (!isDisabled) {
       extCount.pdf += 1;
       $('#extPDF').html(extCount.pdf.toLocaleString());
     }
+    // update institute pdf counter
     portal.pdf += 1;
   }
   if (ec.mime) {
@@ -34,9 +38,11 @@ function counter(ec, portal) {
   if (ec.rtype) {
     ec.rtype = `<span class="label label-bubble rtype">${ec.rtype}</span>`;
   }
+  // initialize the tooltips for each institute when the counter starts (at least one html or pdf)
   if (portal.pdf === 1 || portal.html === 1) {
     $(`#${portal.name}-tooltip`).addClass('tooltipped');
     $(`#${portal.name}-tooltip`).tooltip();
   }
+  // update the tooltips content
   $(`#${portal.name}-tooltip`).attr('data-tooltip', `HTML : ${portal.html} | PDF : ${portal.pdf}`);
 }
